b15ad530e4a1d63b304a3b6a23590b7e9795fdce
Jaylen Yu <114912854+jaylenyu@users.noreply.github.com>
2023-08-21 14:48:42 +0900

    초기세팅

---
+++ src/pages/Main.tsx
@@ -0,0 +1,14 @@
+import React, { FC } from 'react';
+import { Button } from '@class101/ui';
+
+const Main: FC = () => {
+  return (
+    <div>
+      Main
+      <div>portfolio</div>
+      <Button>Button</Button>
+    </div>
+  );
+};
+
+export default Main;
